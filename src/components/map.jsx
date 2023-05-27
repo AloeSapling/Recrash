4b768cfd4ad7d42c8e7d97888f0dd83c2338e41e
KartoszBoza <Bartoszkoza09@gmail.com>
2023-05-27 10:10:23 +0200

    mapa pocztkowa

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,11 +1,16 @@
 import '../css/home.css';
 import '../css/map.css';
-
+import { MapContainer, TileLayer, useMap,Marker, Popup } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css';
 function Map() {
+  
     return (
-      <div className="main">
-        <h1 className='test'>Mapa </h1>
-      </div>
+      <MapContainer className='map' center={[51.505, -0.091]} zoom={13}>
+          <TileLayer
+            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
     );
   }
   
