5112c234321e687a5c59a2cf2e2bdce53d283ad1
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-28 15:05:28 +0200

    zrobilem navbara

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-
+//add md:flex to first ul
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(true);
+
+    const handleToggle = () =>{
+      setToggle(!toggle)
+    }
+
     return ( 
-        <div className='bg-[#71AC57] flex justify-between items-center h-24  mx-auto px-4 text-white'>
-        <img className="w-[266px] h-[90px]" src="./Recrash.png" alt="" />
-        <ul className='hidden md:flex '>
-          <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Home</li>
-          <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Map</li>
-          <li className='p-4  text-3xl font-bold cursor-pointer hover:text-[#38761D]'>About</li>
+      <nav className="fixed w-full">
+        <div className='bg-[#71AC57] flex justify-between items-center h-24 mx-auto px-4 text-white z-50 '>
+        <img className="w-[250px] h-[90px]" src="./Recrash.png" alt="" />
+        <ul className='hidden md:flex'>
+          <Link  to="/"> <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Home</li></Link>
+          <Link to="/map"><li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Map</li></Link>
+          <Link to="/about"><li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>About</li></Link>
         </ul>
 
 
-        <div>
-          <img className="w-[50px] h-[50px]" src="./menu.png" alt="" />
-        </div>
-
-        <div className="fixed right-0 top-0 h-full w-[50%]  text-right">
-          <ul className="pt-[95px]">
-          <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Home</li>
-          <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Map</li>
-          <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>About</li>
-          </ul>
-        </div>
-
+        <div onClick={handleToggle} className="block md:hidden">
+          {!toggle ? <img className="w-[50px] h-[50px] cursor-pointer" src="./close.png" alt="" /> : <img className="w-[50px] h-[50px] cursor-pointer" src="./list.png" alt="" /> }
 
         </div>
+        
+        <div className={ !toggle ? "fixed left-0 top-0 h-full w-[50%] bg-[#71AC57] ease-in-out duration-500 block md:hidden z-50" : 'ease-in-out duration-500 top-0 h-full w-[50%] fixed left-[-100%] z-50'}>
+        <img className=" w-[260px] h-[96px]" src="./Recrash.png" alt="" />
+            <ul className="pt-[95px] uppercase z-50">
+            <Link  to="/"><li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[0px]'>Home</li></Link>
+            <Link to="/map"> <li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]'>Map</li></Link>
+            <Link to="/about"> <li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]'>About</li></Link>
+            </ul> 
+
+        </div> 
+
+
+        </div>
+        </nav>
      );
 }
  
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -2,15 +2,13 @@ import '../css/home.css';
 import '../css/map.css';
 import { MapContainer, TileLayer, useMap,Marker, Popup } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
+import Navbar from './Navbar';
 function Map() {
   
     return (
-      <MapContainer className='map' center={[51.505, -0.091]} zoom={13}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />
-        </MapContainer>
+      <>
+      
+      </>
     );
   }
   
