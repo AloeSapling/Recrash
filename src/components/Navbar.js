0f56bef1c506d8eb6ec723b5b7ccca92e7a31a05
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-28 19:17:37 +0200

    navbara

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
-//add md:flex to first ul
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
 
-    const [toggle, setToggle] = useState(true);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
-    const handleToggle = () =>{
-      setToggle(!toggle)
+  const handleClick = () => {
+    if (!toggle) {
+      setToggle(true); // Hide the navbar on left
     }
+  };
 
-    return ( 
-      <nav className="fixed w-full">
-        <div className='bg-[#71AC57] flex justify-between items-center h-24 mx-auto px-4 text-white z-50 '>
-        <img className="w-[250px] h-[90px]" src="./Recrash.png" alt="" />
-        <ul className='hidden md:flex'>
-          <Link  to="/"> <li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Home</li></Link>
-          <Link to="/map"><li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>Map</li></Link>
-          <Link to="/about"><li className='p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]'>About</li></Link>
+  return (
+    <nav className="w-full">
+      <div className="bg-[#71AC57] flex justify-between items-center h-24 mx-auto px-4 text-white z-50 fixed w-full">
+        <img className="w-[243px] h-[85px]" src="./Recrash.png" alt="" />
+        <ul className="hidden md:flex">
+          <Link to="/" onClick={handleClick}>
+            <li className="p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]">
+              Home
+            </li>
+          </Link>
+          <Link to="/map" onClick={handleClick}>
+            <li className="p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]">
+              Map
+            </li>
+          </Link>
+          <Link to="/about" onClick={handleClick}>
+            <li className="p-4 mr-10 text-3xl font-bold cursor-pointer hover:text-[#38761D]">
+              About
+            </li>
+          </Link>
         </ul>
 
-
         <div onClick={handleToggle} className="block md:hidden">
-          {!toggle ? <img className="w-[50px] h-[50px] cursor-pointer" src="./close.png" alt="" /> : <img className="w-[50px] h-[50px] cursor-pointer" src="./list.png" alt="" /> }
-
+          {!toggle ? (
+            <img className="w-[40px] h-[40px] cursor-pointer" src="./close.png" alt="" />
+          ) : (
+            <img className="w-[40px] h-[40px] cursor-pointer" src="./list.png" alt="" />
+          )}
         </div>
-        
-        <div className={ !toggle ? "fixed left-0 top-0 h-full w-[50%] bg-[#71AC57] ease-in-out duration-500 block md:hidden z-50" : 'ease-in-out duration-500 top-0 h-full w-[50%] fixed left-[-100%] z-50'}>
-        <img className=" w-[260px] h-[96px]" src="./Recrash.png" alt="" />
+
+        <div
+          className={
+            !toggle
+              ? "fixed left-0 top-0 h-full w-[50%] bg-[#71AC57] ease-in-out duration-500 block md:hidden z-50"
+              : "ease-in-out duration-500 top-0 h-full w-[50%]  fixed left-[-100%] z-50"
+          }
+        >
+          <div>
+            <img className="w-[260px] h-[96px]" src="./Recrash.png" alt="" />
+
             <ul className="pt-[95px] uppercase z-50">
-            <Link  to="/"><li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[0px]'>Home</li></Link>
-            <Link to="/map"> <li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]'>Map</li></Link>
-            <Link to="/about"> <li className='pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]'>About</li></Link>
-            </ul> 
-
-        </div> 
-
-
+              <Link to="/" onClick={handleClick}>
+                <li className="pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[0px]">
+                  Home
+                </li>
+              </Link>
+              <Link to="/map" onClick={handleClick}>
+                <li className="pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]">
+                  Map
+                </li>
+              </Link>
+              <Link to="/about" onClick={handleClick}>
+                <li className="pl-4 ml-14 text-3xl border-b border-lime-900 font-bold cursor-pointer hover:text-[#38761D] pt-[70px]">
+                  About
+                </li>
+              </Link>
+            </ul>
+          </div>
         </div>
-        </nav>
-     );
-}
- 
+      </div>
+    </nav>
+  );
+};
+
 export default Navbar;
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -5,12 +5,14 @@ import 'leaflet/dist/leaflet.css';
 import Navbar from './Navbar';
 function Map() {
     return (
+      <>
           <MapContainer className='map' center={[	50.012100,20.985842]} zoom={10}>
             <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
           </MapContainer>
+          </>
     );
   }
   
