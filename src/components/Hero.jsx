58dfffc692dff1f29fcc378c66b0ada666c4a87f
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-29 20:07:57 +0200

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { useState } from "react";
-
+import  './hero.css';
 
 const Hero = () => {
-    return ( 
-        
-        <div className=" mt-[300px] w-full min-h-full h-screen">
-        <img
-          className=" top-0 left-0 w-full h-full "
-          src="./eco img.jpg"
-          alt=""
-        />
-      </div>
-     );
-}
+  return (
+    <div className="">
+      <img
+        className="image "
+        src="./eco img.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
  
 export default Hero;
