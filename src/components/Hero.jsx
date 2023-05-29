e497f1cfb6ac645bcbe5f71e5d272e8aafb055b5
lolkek1234567890 <dima2019mega@gmail.com>
2023-05-29 21:15:19 +0200

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,21 @@ import  './hero.css';
 
 const Hero = () => {
   return (
-    <div className="">
+    <>
+    <div className=" absolute text-center">
       <img
         className="image "
         src="./eco img.jpg"
         alt=""
-      />
-    </div>
+      /> 
+      <h1 className="text1">Sprawdź gdzie</h1>
+      <p className="text2">musisz wyrzucisz papier</p>
+      <p className="text3">Na naszej stronie zobaczysz gdzie warto wyrzucać różne odpady w twoim mieście </p>
+      </div>
+      
+    <div className="z-10 bg-[#486619] h-14 w-4 text-center text-3xl font-bold"/>
+      
+      </>
   );
 };
 
