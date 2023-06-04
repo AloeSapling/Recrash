985559c4d285bd19cd766d3cb53fffcb02fd0f0a
lolkek1234567890 <dima2019mega@gmail.com>
2023-06-04 17:17:31 +0200

    leafs

--- src/components/Sld.js
+++ src/components/Sld.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Slide from 'react-reveal/Slide';
 
+import "./hero.css"
+
 
 const Sld = () => {
     return ( 
-        <>       
+        <>   
+        
+        <img className="leaf1" src="./leafnobg.png" alt="" />
+        <img className="leaf2" src="./leafnobg.png" alt="" />
+
         <Slide top>   
         <div className="w-[100%]">
             <h1 className="flex justify-center mb-[50px] text-[70px] font-bold text-[#3C7339] font-playfire ">Dowiedź się więcej o odpadach</h1>
@@ -19,6 +25,8 @@ const Sld = () => {
                     <img className="row-span-2 col-start-3 row-start-1 w-[225px] h-[225px] mt-[-10px] ml-[35px]" src="./Battery.png" alt="battery" />
                 </div>
             </Slide>
+            <img className="leaf3" src="./leafnobg.png" alt="" />
+            <img className="leaf4" src="./leafnobg.png" alt="" />
             <div className="h-[70px] bg-white w-[5px]" />
             <Slide right className=" ">
             <div className="flex justify-end">
@@ -32,8 +40,11 @@ const Sld = () => {
             </Slide>
             <div className="h-[70px] bg-white w-[5px]" />
             <Slide left>
-                <div className="w-[70%] h-[200px] bg-[#558231] rounded-[45px] ml-[-50px]">
-                    <h1>React Reveal</h1>
+                <div className=" bg-[#558231] text-white w-[70%] h-[200px] rounded-[45px] ml-[-50px] grid grid-cols-3 grid-rows-2 gap-1 ">
+                <h1 className="ml-[100px] mt-[25px] font-playfire text-3xl col-span-2">React Reveal</h1>
+                    <p className="ml-[100px] mt-[-20px] max-w-[750px] col-span-2 col-start-1 row-start-2 ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae minus deserunt magnam, amet quis ut aliquid, nostrum illum esse aliquam optio ducimus eius. Culpa temporibus dolore molestias voluptatum eaque consequatur!
+                    Deleniti voluptas </p>
+                    <img className="row-span-2 col-start-3 row-start-1 w-[225px] h-[225px] mt-[-10px] ml-[35px]" src="./AK.png" alt="Zakr" />
                 </div>
             </Slide>
         </>
