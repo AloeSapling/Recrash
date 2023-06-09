c8d10dee7a32fdf56f78c13b63b12d0a55c3f009
Aloe Sapling <aloesapling@gmail.com>
2023-06-09 16:12:30 +0200

    ikonka+icon

--- src/css/LeafletCSS&IMG/icon.js
+++ src/css/LeafletCSS&IMG/icon.js
@@ -1,9 +1,9 @@
 import L from "leaflet";
 
 export default L.icon({
-  iconSize: [60, 80],
+  iconSize: [30, 50],
   iconAnchor: [29, 80],
   popupAnchor: [2, -40],
-  iconUrl: "https://unpkg.com/browse/leaflet@1.3.4/dist/images/marker-icon.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
   shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
 });
